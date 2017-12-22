1e904abce8f3eaf695a4814397a0b46535fd3d44
Timothy McPhillips <tmcphillips@absoluteflow.org>
2017-12-21 22:16:26 -0800

    Added simple script checking if elasticsearch is up.

---
+++ elasticsearch/is-up.js
@@ -0,0 +1,16 @@
+var elasticsearch = require('elasticsearch');
+
+var client = new elasticsearch.Client({
+  host: 'localhost:9200',
+});
+
+client.ping({
+    // ping usually has a 3000ms timeout
+    requestTimeout: 1000
+  }, function (error) {
+    if (error) {
+      console.log('elasticsearch cluster is down');
+    } else {
+      console.log('all is well');
+    }
+  });
