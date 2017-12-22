779974335c4d9762c746af3b01d0da4c44d263cf
Timothy McPhillips <tmcphillips@absoluteflow.org>
2017-12-22 14:17:10 -0800

    Added first automated tests of elasticsearch.
    Tests use Jest and Frisby.

---
+++ elasticsearch/src/__tests__/es.test.js
@@ -0,0 +1,45 @@
+const frisby = require('frisby');
+
+describe("When elasticsearch is running", () => {
+
+        it('GET / should return cluster name', function (done) {
+            frisby.get('http://localhost:9200/')
+            .expect('status', 200)
+            .expect('json', {"cluster_name": "skope-es-cluster-dev" })
+            .done(done);
+        });
+
+        it('PUT _cluster/health should return cluster name', function (done) {
+            frisby.get('http://localhost:9200/_cluster/health')
+            .expect('json', {"cluster_name": "skope-es-cluster-dev" })
+            .done(done);
+        });
+});
+
+describe("When a new document is added to elasticsearch", () => {
+
+
+    it ('PUT new document should return a status of 201', function (done) {
+
+        frisby.del('http://localhost:9200/megacorp').done(done);
+
+        frisby.put('http://localhost:9200/megacorp/employee/12', 
+                {
+                "first_name" : "John",
+                "last_name" :  "Smith",
+                "age" :        25,
+                "about" :      "I love to go rock climbing",
+                "interests": [ "sports", "music" ]
+                }
+            )
+            .expect('status', 201)
+            .expect('json', {"_index": "megacorp" })
+            .done(done);
+
+        // frisby.get('http://localhost:9200/megacorp/employee/12')
+        // .expect('status', 200)
+        // // .expect('json', "")
+        // .done(done);
+        // })
+    });
+});
