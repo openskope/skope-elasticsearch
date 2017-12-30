44ac78a8319d25b8fe7db81ccc73731cd326ce51
Timothy McPhillips <tmcphillips@absoluteflow.org>
2017-12-29 16:53:40 -0800

    Added tests of searches that return all documents in an index.

--- elasticsearch/src/__tests__/es.rest.test.js
+++ elasticsearch/src/__tests__/es.rest.test.js
@@ -3,7 +3,7 @@ const rest = require('rest');
 const mime = require('rest/interceptor/mime');
 
 const esClusterUrl = 'http://localhost:9200';
-
+  
 const callRESTService =  rest.wrap(mime, { mime: 'application/json' } );
 
 describe("When elasticsearch is running", () => {
@@ -121,6 +121,7 @@ describe("When a document exists in elasticsearch", async () => {
 });
 
 describe("When a single document is retrieved from elasticsearch using GET", async () => {
+
     var response;
 
     var document = {
@@ -164,4 +165,135 @@ describe("When a single document is retrieved from elasticsearch using GET", asy
     });
 
 });
+
+describe("When a single document is found via search for all documents in an index", async () => {
     
+    var response;
+
+    var document = {
+        "first_name" : "John",
+        "last_name" :  "Smith",
+        "age" :        25,
+        "about" :      "I love to go rock climbing",
+        "interests": [ "sports", "music" ]
+    };
+
+    beforeAll(async () => {
+        
+        await es.deleteIndex(esClusterUrl, 'megacorp');
+
+        await callRESTService({
+            method: 'POST', 
+            path: esClusterUrl + '/megacorp/employee/1',
+            entity: document
+        });
+
+        await es.refreshAll(esClusterUrl);
+
+        response = await callRESTService({
+            method: 'GET',
+            path: esClusterUrl + '/megacorp/_search'
+        });
+    });
+
+    it ('the returned http status code should be 200 - OK', async () => {
+        expect(response.status.code).toBe(200);
+    });
+
+    it ('the total number of search hits should be 1', async () => {
+        expect(response.entity.hits.total).toBe(1);
+    });
+
+    it ('the maximum score of the search hits should be 1', async () => {
+        expect(response.entity.hits.max_score).toBe(1);
+    });
+
+    it ('the score of the single hit should be 1', async () => {
+        expect(response.entity.hits.hits[0]._score).toBe(1);
+    });
+
+    it ('the returned metadata for the single hit should match that of the document', async () => {
+        expect(response.entity.hits.hits[0]._index).toBe('megacorp');
+        expect(response.entity.hits.hits[0]._type).toBe('employee');
+        expect(response.entity.hits.hits[0]._id).toBe('1');
+    });
+
+    it ('the returned document matches that stored in elasticsearch', async () => {
+        expect(response.entity.hits.hits[0]._source).toEqual(document);
+    });
+
+});
+
+
+describe("When 3 documents are found via search for all documents in an index", async () => {
+    
+    var response;
+
+    var document = [{
+        "first_name" : "John",
+        "last_name" :  "Smith",
+        "age" :        25,
+        "about" :      "I love to go rock climbing",
+        "interests": [ "sports", "music" ]
+    }, {
+        "first_name" :  "Jane",
+        "last_name" :   "Smith",
+        "age" :         32,
+        "about" :       "I like to collect rock albums",
+        "interests":  [ "music" ]
+    }, {
+        "first_name" :  "Douglas",
+        "last_name" :   "Fir",
+        "age" :         35,
+        "about":        "I like to build cabinets",
+        "interests":  [ "forestry" ]
+    }];
+
+    beforeAll(async () => {
+        
+        await es.deleteIndex(esClusterUrl, 'megacorp');
+
+        await callRESTService({
+            method: 'POST', 
+            path: esClusterUrl + '/megacorp/employee/0',
+            entity: document[0]
+        });
+
+        await callRESTService({
+            method: 'POST', 
+            path: esClusterUrl + '/megacorp/employee/1',
+            entity: document[1]
+        });
+
+        await callRESTService({
+            method: 'POST', 
+            path: esClusterUrl + '/megacorp/employee/2',
+            entity: document[2]
+        });
+
+        await es.refreshAll(esClusterUrl);
+
+        response = await callRESTService({
+            method: 'GET',
+            path: esClusterUrl + '/megacorp/_search'
+        });
+    });
+
+    it ('the returned http status code should be 200 - OK', async () => {
+        expect(response.status.code).toBe(200);
+    });
+
+    it ('the total number of search hits should be 3', async () => {
+        expect(response.entity.hits.total).toBe(3);
+    });
+
+    it ('the maximum score of the search hits should be 1', async () => {
+        expect(response.entity.hits.max_score).toBe(1);
+    });
+
+    it ('the score of each hit should be 1', async () => {
+        expect(response.entity.hits.hits[0]._score).toBe(1);
+        expect(response.entity.hits.hits[1]._score).toBe(1);
+        expect(response.entity.hits.hits[2]._score).toBe(1);
+    });
+});
