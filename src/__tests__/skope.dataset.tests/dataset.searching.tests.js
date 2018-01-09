2ff265dcbcdc8118bdbb5c4f86da1377eaa0b90e
Timothy McPhillips <tmcphillips@absoluteflow.org>
2018-01-08 19:16:15 -0800

    Test search by study.id and by model.nickname.

--- src/__tests__/skope.dataset.tests/dataset.searching.tests.js
+++ src/__tests__/skope.dataset.tests/dataset.searching.tests.js
@@ -33,7 +33,7 @@ beforeAll(async () => {
     await es.refreshAll(esClusterUrl);
 });
 
-describe("When all datasets are found via search", async () => {
+describe("When all datasets are found via search for all documents in the index", async () => {
     
     var response;
 
@@ -58,9 +58,86 @@ describe("When all datasets are found via search", async () => {
     });
 
     it ('the score of each hit should be 1', async () => {
-        for (var i = 0; i < 2; i += 1) {
+        for (var i = 0; i < response.entity.hits.total; i += 1) {
             expect(response.entity.hits.hits[i]._score).toBe(1);
         }
+    });
+
+});
+
+describe("When 2 documents are found via full text search of study.id field", async () => {
+    
+    var response;
+    var hits;
+    
+    beforeAll(async () => {
+
+        response = await callRESTService({
+            method: 'GET',
+            path: esClusterUrl + '/skope/_search',
+            entity: {
+                "query" : {
+                    "match" : {
+                        "study.id" : "19783"
+                    }
+                }
+            }
+        });
+
+        hits = response.entity.hits.hits;
+    });
+
+    it ('the returned http status code should be 200 - OK', async () => {
+        expect(response.status.code).toBe(200);
+    });
+
+    it ('the total number of search hits should be 2', async () => {
+        expect(response.entity.hits.total).toBe(2);
+    });
+});
+
+describe("When documents are searched by model.nickname keyword field", async () => {
+    
+    var matchingSearchResponse;
+    var nonMatchingSearchResponse;
+    var hits;
+    
+    beforeAll(async () => {
+
+        matchingSearchResponse = await callRESTService({
+            method: 'GET',
+            path: esClusterUrl + '/skope/_search',
+            entity: {
+                "query" : {
+                    "match" : {
+                        "model.nickname" : "PaleoCAR"
+                    }
+                }
+            }
+        });
+
+        nonMatchingSearchResponse = await callRESTService({
+            method: 'GET',
+            path: esClusterUrl + '/skope/_search',
+            entity: {
+                "query" : {
+                    "match" : {
+                        "model.nickname" : "PaleoCA"
+                    }
+                }
+            }
+        });
+
+    });
+
+    it ('the returned http status codes should be 200 - OK', async () => {
+        expect(matchingSearchResponse.status.code).toBe(200);
+        expect(nonMatchingSearchResponse.status.code).toBe(200);
+    });
+
+    it ('the total number of search hits should be 2', async () => {
+        expect(matchingSearchResponse.entity.hits.total).toBe(2);
+        expect(nonMatchingSearchResponse.entity.hits.total).toBe(0);
     });
 
 });
